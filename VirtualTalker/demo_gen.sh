89671c267f71e1623fb4a1bf3daecb45da56093f
LCMonitor <13723423785@163.com>
2025-08-02 19:51:02 +0800

    完善了错误提示，修复了脚本路径问题

--- VirtualTalker/demo_gen.sh
+++
@@ -1,9 +0,0 @@
-#修改运行路径到SadTalker目录
-cd ./VirtualTalker/SadTalker
-
-source ./venv/bin/activate
-
-python ./inference.py --driven_audio ../test_audio_10s.wav \
-                    --source_image ../avatar.png \
-                    --still \
-                    --preprocess full 
